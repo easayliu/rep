1394684bc49fd205e6e7cf4d4efdddb9fd72939b
liuyixun <liuyixun@afanticar.com>
2024-02-02 11:23:21 +0800

    ad sg-01

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
 wget https://github.com/XrayR-project/XrayR/releases/download/v0.9.1/XrayR-linux-64.zip
 unzip XrayR-linux-64.zip  -d xrayr
 cd xrayr/
+
+nohup ./XrayR -config config.yml > 1.log 2>&1 &
